ef706d3d99eb8f85d40ace43af9dcfa63aaf3429
alessandropelayo <154472296+alessandropelayo@users.noreply.github.com>
2024-05-28 17:17:52 -0700

    SystemUI sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,41 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ThemeContent, { useTheme } from "@/context/theme";
+import * as SystemUI from "expo-system-ui";
 
 export default function RootLayout() {
-	
 	return (
-		<SafeAreaProvider>
-			<ThemeContent>
-				<RootStack/>
-			</ThemeContent>
-		</SafeAreaProvider>
+		<ThemeContent>
+			<SafeAreaProvider>
+				<RootStack />
+			</SafeAreaProvider>
+		</ThemeContent>
 	);
 }
 
 function RootStack() {
-
 	const theme = useTheme().currentTheme;
+	SystemUI.setBackgroundColorAsync(theme.background);
 
-	return (<Stack
-		screenOptions={{
-			headerStyle: {
-				backgroundColor: theme.background,
-			},
-			headerTitleStyle: {
-				color: theme.text,
-			},
-			headerTintColor: theme.tint,
-			navigationBarColor: theme.foreground,
-		}}
-	>
-		<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-		<Stack.Screen name="Settings" options={{ headerShown: true, navigationBarColor: theme.background }} />
-		<Stack.Screen name="+not-found" />
-	</Stack>);
+	return (
+		<Stack
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: theme.background,
+				},
+				headerTitleStyle: {
+					color: theme.text,
+				},
+				headerTintColor: theme.tint,
+				navigationBarColor: theme.foreground,
+			}}
+		>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen
+				name="Settings"
+				options={{ headerShown: true, navigationBarColor: theme.background }}
+			/>
+			<Stack.Screen name="+not-found" />
+		</Stack>
+	);
 }
